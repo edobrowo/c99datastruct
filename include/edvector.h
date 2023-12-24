40868847142587d853304c66043277dba53090b9
Ethan Dobrowolski <ethan.dobrowolski@gmail.com>
2023-12-23 23:06:05 -0500

    Complete vector implementation

---
+++ include/edvector.h
@@ -0,0 +1,101 @@
+#ifndef DATASTRUCTURES_VECTOR_H
+#define DATASTRUCTURES_VECTOR_H
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define VEC_MIN_CAP 10
+#define VEC_INITIAL_SIZE VEC_MIN_CAP
+
+typedef struct _vector_t {
+    void* data;
+    size_t data_size;
+    size_t size;
+    size_t _cap;
+} vector_t;
+
+// Resizes a vector according to doubling strategy
+static inline void _vec_resize(vector_t* vec) {
+    assert(vec != NULL);
+    if (vec->_cap > VEC_MIN_CAP && vec->size < vec->_cap / 2) {
+        vec->_cap /= 2;
+        vec->data = realloc(vec->data, vec->_cap * sizeof(vec->data_size));
+    } else if (vec->size >= vec->_cap) {
+        vec->_cap *= 2;
+        vec->data = realloc(vec->data, vec->_cap * sizeof(vec->data_size));
+    }
+}
+
+// Creates a new heap-allocated vector
+vector_t* vec_new(const size_t data_size) {
+    vector_t* vec = (vector_t*)malloc(sizeof(vector_t));
+    vec->data_size = data_size;
+    vec->data = malloc(vec->_cap * vec->data_size);
+    vec->size = 0;
+    vec->_cap = VEC_INITIAL_SIZE;
+    return vec;
+}
+
+// Destructs a vector
+void vec_del(vector_t* vec) {
+    if (vec == NULL)
+        return;
+    free(vec->data);
+    free(vec);
+}
+
+// Indicates if vec is empty
+int vec_empty(const vector_t* vec) {
+    assert(vec != NULL);
+    return vec->size == 0;
+}
+
+// Returns the object at index i without bounds check
+void* vec_at(const vector_t* vec, const size_t i) {
+    assert(vec != NULL);
+    return (char*)vec->data + i * vec->data_size;
+}
+
+// Returns the object at index i with bounds check
+void* vec_atc(const vector_t* vec, const size_t i) {
+    assert(vec != NULL);
+    if (i >= vec->size) {
+        return NULL;
+    }
+    return (char*)vec->data + i * vec->data_size;
+}
+
+// Adds a new element to the end of vec
+void vec_pushb(vector_t* vec, const void* element) {
+    assert(vec != NULL);
+    _vec_resize(vec);
+    void* end = (char*)vec->data + vec->size * vec->data_size;
+    memcpy(end, element, vec->data_size);
+    ++vec->size;
+}
+
+// Removes an element from vec at index i
+void vec_remove(vector_t* vec, const size_t i) {
+    assert(vec != NULL);
+    if (i >= vec->size)
+        return;
+    void* start = (char*)vec->data + i * vec->data_size;
+    memcpy(start, (char*)start + vec->data_size, (vec->size - i) * vec->data_size);
+    --vec->size;
+    _vec_resize(vec);
+}
+
+// Inserts an element elem into vec at index i
+void vec_insert(vector_t* vec, const size_t i, const void* element) {
+    assert(vec != NULL);
+    if (i > vec->size)
+        return;
+    _vec_resize(vec);
+    void* start = (char*)vec->data + i * vec->data_size;
+    memcpy((char*)start + vec->data_size, start, (vec->size - i) * vec->data_size);
+    memcpy(start, element, vec->data_size);
+    ++vec->size;
+}
+
+#endif  // DATASTRUCTURES_VECTOR_H
--- src/main.c
+++ src/main.c
@@ -1,11 +1,111 @@
+#include <assert.h>
 #include <stdio.h>
 
-#include "vector.h"
+#include "edvector.h"
 
 void vector_test() {
-    //
+    vector_t* vec = vec_new(sizeof(int));
+    int v[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    vec_pushb(vec, (void*)v);
+    vec_pushb(vec, (void*)(v + 1));
+    vec_pushb(vec, (void*)(v + 2));
+    vec_pushb(vec, (void*)(v + 3));
+    vec_pushb(vec, (void*)(v + 4));
+    vec_pushb(vec, (void*)(v + 5));
+    vec_pushb(vec, (void*)(v + 6));
+    vec_pushb(vec, (void*)(v + 7));
+    vec_pushb(vec, (void*)(v + 8));
+    vec_pushb(vec, (void*)(v + 9));
+    assert(!vec_empty(vec));
+    assert(vec->size == 10);
+    assert(vec->_cap == 10);
+    assert(*(int*)vec_at(vec, 0) == 1);
+    assert(*(int*)vec_at(vec, 1) == 2);
+    assert(*(int*)vec_at(vec, 2) == 3);
+    assert(*(int*)vec_at(vec, 3) == 4);
+    assert(*(int*)vec_at(vec, 4) == 5);
+    assert(*(int*)vec_at(vec, 5) == 6);
+    assert(*(int*)vec_at(vec, 6) == 7);
+    assert(*(int*)vec_at(vec, 7) == 8);
+    assert(*(int*)vec_at(vec, 8) == 9);
+    assert(*(int*)vec_at(vec, 9) == 10);
+    assert(*(int*)vec_atc(vec, 0) == 1);
+    assert(*(int*)vec_atc(vec, 1) == 2);
+    assert(*(int*)vec_atc(vec, 2) == 3);
+    assert(*(int*)vec_atc(vec, 9) == 10);
+    assert(vec_atc(vec, 10) == NULL);
+    vec_remove(vec, 0);
+    vec_remove(vec, 0);
+    vec_remove(vec, 5);
+    assert(!vec_empty(vec));
+    assert(vec->size == 7);
+    assert(vec->_cap == 10);
+    int ins = 20;
+    vec_insert(vec, 3, (void*)&ins);
+    vec_insert(vec, 3, (void*)&ins);
+    assert(*(int*)vec_at(vec, 0) == 3);
+    assert(*(int*)vec_at(vec, 1) == 4);
+    assert(*(int*)vec_at(vec, 2) == 5);
+    assert(*(int*)vec_at(vec, 3) == 20);
+    assert(*(int*)vec_at(vec, 4) == 20);
+    assert(*(int*)vec_at(vec, 5) == 6);
+    assert(*(int*)vec_at(vec, 6) == 7);
+    assert(*(int*)vec_at(vec, 7) == 9);
+    assert(*(int*)vec_at(vec, 8) == 10);
+    vec_insert(vec, 0, (void*)&ins);
+    vec_insert(vec, 10, (void*)&ins);
+    assert(!vec_empty(vec));
+    assert(vec->size == 11);
+    assert(vec->_cap == 20);
+    assert(*(int*)vec_at(vec, 0) == 20);
+    assert(*(int*)vec_at(vec, 1) == 3);
+    assert(*(int*)vec_at(vec, 2) == 4);
+    assert(*(int*)vec_at(vec, 3) == 5);
+    assert(*(int*)vec_at(vec, 4) == 20);
+    assert(*(int*)vec_at(vec, 5) == 20);
+    assert(*(int*)vec_at(vec, 6) == 6);
+    assert(*(int*)vec_at(vec, 7) == 7);
+    assert(*(int*)vec_at(vec, 8) == 9);
+    assert(*(int*)vec_at(vec, 9) == 10);
+    assert(*(int*)vec_at(vec, 10) == 20);
+    vec_remove(vec, 0);
+    assert(vec->size == 10);
+    assert(vec->_cap == 20);
+    vec_remove(vec, 9);
+    assert(*(int*)vec_at(vec, 0) == 3);
+    assert(*(int*)vec_at(vec, 1) == 4);
+    assert(*(int*)vec_at(vec, 2) == 5);
+    assert(*(int*)vec_at(vec, 3) == 20);
+    assert(*(int*)vec_at(vec, 4) == 20);
+    assert(*(int*)vec_at(vec, 5) == 6);
+    assert(*(int*)vec_at(vec, 6) == 7);
+    assert(*(int*)vec_at(vec, 7) == 9);
+    assert(*(int*)vec_at(vec, 8) == 10);
+    vec_remove(vec, 0);
+    vec_remove(vec, 0);
+    vec_remove(vec, 0);
+    vec_remove(vec, 1);
+    assert(*(int*)vec_at(vec, 0) == 20);
+    assert(*(int*)vec_at(vec, 1) == 6);
+    assert(*(int*)vec_at(vec, 2) == 7);
+    assert(*(int*)vec_at(vec, 3) == 9);
+    assert(*(int*)vec_at(vec, 4) == 10);
+    assert(vec->size == 5);
+    assert(vec->_cap == 10);
+    vec_remove(vec, 4);
+    assert(*(int*)vec_at(vec, 0) == 20);
+    assert(*(int*)vec_at(vec, 1) == 6);
+    assert(*(int*)vec_at(vec, 2) == 7);
+    assert(*(int*)vec_at(vec, 3) == 9);
+    assert(vec->size == 4);
+    assert(vec->_cap == 10);
+    vec_remove(vec, 3);
+    assert(vec->size == 3);
+    assert(vec->_cap == 10);
+    vec_del(vec);
 }
 
 int main() {
-    printf("Hello world!");
+    vector_test();
 }
